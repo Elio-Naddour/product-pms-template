a2c837f8cd9d903f3a001d4747d54aad2340dd42
elio <enaddour961@gmail.com>
2025-01-02 10:49:38 +0200

    active-links-in-header

--- src/components/molecules/header/Header.tsx
+++ src/components/molecules/header/Header.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 import routeNames from 'src/utils/routeNames';
 
 const Header: React.FC = () => {
+  const location = useLocation();
+  const [activePath, setActivePath] = useState<string>(location.pathname);
+
+  useEffect(() => {
+    setActivePath(location.pathname);
+  }, [location]);
+
   return (
     <Navbar expand='lg' className='custom-navbar'>
       <Container className='nav-container'>
         <Navbar.Brand href={routeNames.home}>Elio PMS</Navbar.Brand>
         <Nav className='links-container'>
-          {/* <Nav.Link href={routeNames.home}>Home</Nav.Link> */}
-          <Nav.Link href={routeNames.products}>Products</Nav.Link>
-          <Nav.Link href={routeNames.about}>About</Nav.Link>
+          <Nav.Link
+            href={routeNames.home}
+            active={activePath === routeNames.home}
+            className={activePath === routeNames.home ? 'active-link' : ''}
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link
+            href={routeNames.products}
+            active={activePath === routeNames.products}
+            className={activePath === routeNames.products ? 'active-link' : ''}
+          >
+            Products
+          </Nav.Link>
+          <Nav.Link
+            href={routeNames.about}
+            active={activePath === routeNames.about}
+            className={activePath === routeNames.about ? 'active-link' : ''}
+          >
+            About
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
